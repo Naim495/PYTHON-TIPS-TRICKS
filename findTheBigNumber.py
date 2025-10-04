d842314e2afa75478601b37e697e743cfe4e2230
Naimur Rahman Emon <016112naimrahman@gmail.com>
2025-10-04 22:30:27 +0600

    Create findTheBigNumber.py

---
+++ findTheBigNumber.py
@@ -0,0 +1,14 @@
+a=int(input("enter 1st number: "))
+b=int(input("enter 2nd number: "))
+c=int(input("enter 3rd number: "))
+
+if a>b:
+      if a>c:
+            print(f"{a} is the bigest number")
+      else :
+            print(f"{c} is the bigist number ")
+elif b>a:
+      if b>c:
+            print(f"{b} is the bigist number")
+      else :
+            print(f"{c} is the biggist number")      
