63efa46db9a367a2e10d04db7794d92d31814891
Naimur Rahman Emon <016112naimrahman@gmail.com>
2025-10-04 23:13:54 +0600

    Update if_else_ternary.py

--- if_else_ternary.py
+++ if_else_ternary.py
@@ -4,3 +4,10 @@ c = int(input("Enter c: "))
 
 biggest = a if (a > b and a > c) else b if b > c else c
 print(f"The biggest number is {biggest}")
+//finding the min number :
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+smallest = a if (a < b and a < c) else b if b < c else c
+print(f"The smallest number is {smallest}")
